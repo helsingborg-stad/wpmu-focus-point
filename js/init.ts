1636c04e843cb6de701da30c489f441d651694f1
NiclasNorin <niclasn@outlook.com>
2025-04-02 10:46:15 +0200

    fix: only run for images

--- js/init.ts
+++ js/init.ts
@@ -12,6 +12,18 @@ document.addEventListener("DOMContentLoaded", () => {
             const image = document.querySelector(`img.details-image:not(.icon)`) as HTMLImageElement;
 
             if (image && focusX && focusY) {
+                const attachmentId = focusX.dataset.attachmentId;
+                const attachment = wp.media.attachment(attachmentId);
+                console.log(attachment.get("mime"));
+                if (
+                    !attachment ||
+                    !attachment.get('mime') ||
+                    !attachment.get('mime').includes("image") ||
+                    attachment.get('mime').includes("image/svg+xml")
+                ) {
+                    return;
+                }
+                console.log(attachment);
                 new Main(image, focusX, focusY);
             }
         }, 300);
